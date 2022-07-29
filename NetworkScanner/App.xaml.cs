eb841656c08e3bb4e050331499dc03b454ced834
Damian Viktorovich Morozov <damian@morozov33.ru>
2022-07-29 12:07:43 +0300

    Updated NetworkScanner

--- NetworkScanner/App.xaml.cs
+++ NetworkScanner/App.xaml.cs
@@ -1,12 +1,20 @@
-﻿namespace NetworkScanner
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
-            MainPage = new AppShell();
-        }
-    }
+namespace NetworkScanner;
+
+/// <summary>
+/// Main app.
+/// </summary>
+public partial class App : Application
+{
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
 }
